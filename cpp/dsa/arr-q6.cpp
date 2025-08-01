e19ccebf605a6e85202b804692b5779d8e4ffcc8
Aryan Chaturvedi <aryanchaturvedi.k@gmail.com>
2025-08-02 02:19:41 +0530

    1/8/25

--- cpp/dsa/arr-q6.cpp
+++ cpp/dsa/arr-q6.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 using namespace std;
 
-void rev(int* arr, int low,int n){
-    for(int i=low;i<n/2;i++){
-        swap(arr[i],arr[n-i-1]);
+void rev(int *arr, int low,int high){
+    while(low<high){
+        swap(arr[low],arr[high]);
+        low++;
+        high--;
     }
 }
+
 int main() {
     int n;
     cin >> n;
@@ -15,35 +18,35 @@ int main() {
     int d;
     cin>>d;
     //brute 
-    // for(int i=0;i<d%n;i++){
-    //     int temp=0;
-    //     temp=arr[0];
-    //     for(int j=0;j<n;j++){
-    //         arr[j]=arr[j+1];
-    //     }
-    //     arr[n-1]=temp;
-    // }
+    for(int i=0;i<d%n;i++){
+        int temp=0;
+        temp=arr[0];
+        for(int j=0;j<n;j++){
+            arr[j]=arr[j+1];
+        }
+        arr[n-1]=temp;
+    }
 
-    // for(auto i:arr){
-    //     cout<<i<<" ";
-    // }
+    for(auto i:arr){
+        cout<<i<<" ";
+    }
         //OR
-    // int temp[d];
-    // for(int i=0;i<d;i++){
-    //    temp[i]=arr[i];
-    // }
-    // for(int i=d;i<n;i++){
-    //     arr[i-d]=arr[i];
-    // }
-    // for(int i=n-d;i<n;i++){
-    //     arr[i]=temp[i-n+d];
-    // }
-    // for(auto i:arr) cout<<i<<" ";
+    int temp[d];
+    for(int i=0;i<d;i++){
+       temp[i]=arr[i];
+    }
+    for(int i=d;i<n;i++){
+        arr[i-d]=arr[i];
+    }
+    for(int i=n-d;i<n;i++){
+        arr[i]=temp[i-n+d];
+    }
+    for(auto i:arr) cout<<i<<" ";
 
     //optimal
-    rev(arr,0,d);
-    //rev(arr,d,n);
-    rev(arr,0,n);
-
+    //TC=O(N), SC=O(1)
+    rev(arr,0,d-1);
+    rev(arr,d,n-1);
+    rev(arr,0,n-1);
     for(auto i:arr) cout<<i<<" ";
 }
