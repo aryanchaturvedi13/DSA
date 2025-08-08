6be3c20e3985544a8266678308b042764a496018
Aryan Chaturvedi <aryanchaturvedi.k@gmail.com>
2025-08-08 23:27:27 +0530

    8/8/25

---
+++ cpp/leetcode/qn.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    // class Solution {
+    // public:
+    // int maxArea(vector<int>& height) {
+        vector<int>height={1,6,8,4};
+        int hmax=0;
+        int x=0;
+        for(int i=0;i<height.size();i++){
+            if(height[i]>hmax) x=i;
+            hmax=max(height[i],hmax);
+        }
+        int area=0;
+        for(int i=0;i<height.size();i++){
+            area=max(area,hmax*(x-i));
+        }
+        cout<<area;
+
+    }
