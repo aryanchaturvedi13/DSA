833f682f02c0376349ea4149e8e3c4015c06c44b
Aryan Chaturvedi <aryanchaturvedi.k@gmail.com>
2025-08-20 15:10:53 +0530

    stack

--- cpp/stack and queue/stack.cpp
+++ cpp/stack and queue/stack.cpp
@@ -5,28 +5,28 @@ int st[10];
 
 //using arrays
 class stImpl1{
-    int topIdx = -1;
+    int topidx = -1;
 public:
     void print(){
-        int i=topIdx;
+        int i=topidx;
         while(i>=0){
             cout<<st[i]<<" ";
             i--;
         }
     }
     void push(int x){
-        topIdx = topIdx + 1;
-        st[topIdx] = x;
+        topidx = topidx + 1;
+        st[topidx] = x;
     }
-    int peek(){
-        if(topIdx == -1) return 0;
-        return st[topIdx];
+    int top(){
+        if(topidx == -1) return 0;
+        return st[topidx];
     }
     int size(){
-        return topIdx + 1;
+        return topidx + 1;
     }
     void pop(){
-        topIdx = topIdx - 1;
+        topidx = topidx - 1;
     }
     bool isempty(){
         for(auto i:st){
@@ -43,15 +43,16 @@ public:
 
 void rev_arr(int arr[]){
     stImpl1 st;
+    int top=0;
     for(int i=0;i<10;i++){
         st.push(arr[i]);
-    }
-    int top=-1;
-    for(auto i:st){
         top++;
     }
-
-
+    for(int i=0;i<10;i++){
+        int k=st.top();
+        arr[i]=k;
+        st.pop();
+    }
 }
 
 
@@ -71,7 +72,7 @@ public:
         st.push_back(x);
         topidx=topidx+1;
     }
-    int peek(){
+    int top(){
         if(topidx==-1) return 0;
         return st[topidx];
     }
@@ -108,7 +109,7 @@ public:
         high->next=temp;
         high=high->next;
     }
-    int peek(){
+    int top(){
         if(head==nullptr) return 0;
         return high->data;
     }
@@ -132,17 +133,81 @@ public:
         delete mover;
         mover=nullptr;
     }
+};
+
+//for strings 
+class stImpl4{
+    node* high=new node(0);
+    node* head=high;
+public:
+    void printst(){
+        head=rev(head);
+        node* temp=head;
+        while(temp->next!=nullptr){
+            cout<<temp->data<<" ";
+            temp=temp->next;
+        }
+        cout<<endl;
+    }
+    bool isempty(){
+        if(head==nullptr) return 1;
+        else return 0;
+    }
+    void push(int x){
+        node* temp=new node(x);
+        high->next=temp;
+        high=high->next;
+    }
+    int top(){
+        if(head==nullptr) return 0;
+        return high->data;
+    }
+    int size(){
+        if(head==nullptr) return 0;
+        node* temp=head;
+        int i=1;
+        while(temp){
+            i++;
+            temp=temp->next;
+        }
+        return i;
+    }
+    void pop(){
+        node* temp=head;
+        while(temp->next){
+            temp=temp->next;
+        }
+        node* mover=high;
+        high=temp;
+        delete mover;
+        mover=nullptr;
+    }
+    bool ispalin(){
+        node* head2=rev(head);
+        node* temp1=head;
+        node* temp2=head2;
+        while(temp1 && temp2){
+            if(temp1->data!=temp2->data) return false;
+        }
+        return true;
+    }
 
 
 };
 
 
 int main(){
-    int arr[] = {2, 4, 5, 68, 1,6};
-    stImpl1 st;
-    for(auto i: arr){
-        st.push(i);
-    }
-    // st.rev_arr();
-    st.print();
+    // int arr[] = {2, 4, 5, 68, 1,6,5,7,9,10};
+    // stImpl1 st;
+    // for(auto i: arr){
+    //     st.push(i);
+    // }
+    // rev_arr(arr);
+    // for(auto i:arr) cout<<i<<" ";
+    // st.print();
+    char arr[]={'a','f','g','g','f','a'};
+    stImpl4 st;
+    for(auto i:arr) st.push(i);
+    st.ispalin();
+
 }
